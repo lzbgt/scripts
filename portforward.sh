3f54f26fbf6971714f71c8528821d17f95e9c8d1
lzbgt@126.com <lzbgt@126.com>
2015-07-21 16:01:45 +0800

    revise

--- portforward.sh
+++ portforward.sh
@@ -2,24 +2,31 @@
 
 ###
 # tools for forward local port to remote host
-# Bruce.Lu <rikusouhou@gmail.com> 2015/07/20
+# Bruce.Lu <rikusouhou@gmail.com> 2015
 
-SERVERADDR=tts2.zjqunshuo.com:8080
-PORT=80
+basename $0
+
+TARGETADDR=9922:zjqs.1ding.me:22
+PROXYIP=0.0.0.0
+REVERT=0
 if [ $# -ne 0 ]
 then
     if [ "$1" != "" ]
     then
-        PORT=$1
+        TARGETADDR=$1
     fi
     if [ "$2" != "" ]
     then
-        SERVERADDR=$2
+        PROXYIP=$2
+    fi
+    if [ "$3" != "1" ]
+    then
+        REVERT=1
     fi
 fi
 
 PWD=`pwd`
-PROJ=.forward-tts
+PROJ=.portmapping
 
 #cron
 CRONFILE=cron.tmp
@@ -33,11 +40,18 @@ rm -fr $CRONFILE
 rm -fr $PROJ.sh
 tee $PROJ.sh << PROJSH >/dev/null 2>&1
 #!/bin/sh
-EXISTED=\`netstat -antp|grep ssh|grep -wc $PORT\`
+EXISTED=\`netstat -antp|grep ssh|grep -c "$TARGETADDR"\`
 if [ "\$EXISTED" == "0" ]
 then
-ssh -L $PORT:$SERVERADDR -fgN 0.0.0.0
+    if [ "$REVERT" == "1" ]
+    then
+    ssh -R $TARGETADDR -fgN $PROXYIP 
+    else
+    ssh -L $TARGETADDR -fgN $PROXYIP
+    fi
 fi
+
 PROJSH
+
 chmod +x $PROJ.sh
 sh $PROJ.sh
